142a25aecb88737182d9457e6435818e3f672486
HUECTRUM <lolihatedat1@gmail.com>
2024-07-28 02:26:57 +0200

    template

---
+++ template.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_set>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <fstream>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bitset>
+#include <sstream>
+#include <ext/rope>
+#include <ctime>
+#include <random>
+#include <cstdlib>
+#include <complex>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpii vector<pair<int, int>>
+#define vpii vector<pair<int, int>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define vd vector<double>
+#define vvd vector<vector<double>>
+
+
+/* FUNCTIONS */
+#define feach(el, v) for(auto &el: v)
+#define rep(i, n) for(int i=0;i<n;i++)
+#define reprv(i, n) for(int i=n-1;i>=0;i--)
+#define reps(i, s, e) for(int i=s;i<e;i++)
+#define reprve(i, e, s) for(int i=e-1;i>=s;i--)
+#define repe(x, y) for (auto &x: y)
+#define repe2(x, a, y) for (auto &[x,a]: y)
+
+
+const ll mod = 998244353;
+
+template<ll mod = 998244353>
+struct ModInt {
+    ll p;
+
+    ModInt() : p(0) {}
+
+    ModInt(ll x) { p = x >= 0 ? x % mod : x + (-x + mod - 1) / mod * mod; }
+
+    ModInt &operator+=(const ModInt &y) {
+        p = p + *y - ((p + *y) >= mod ? mod : 0);
+        return *this;
+    }
+
+    ModInt &operator-=(const ModInt &y) {
+        p = p - *y + (p - *y < 0 ? mod : 0);
+        return *this;
+    }
+
+    ModInt &operator*=(const ModInt &y) {
+        p = (p * *y) % mod;
+        return *this;
+    }
+
+    ModInt &operator%=(const ModInt &y) {
+        if (y)p %= *y;
+        return *this;
+    }
+
+    ModInt operator+(const ModInt &y) const {
+        ModInt x = *this;
+        return x += y;
+    }
+
+    ModInt operator-(const ModInt &y) const {
+        ModInt x = *this;
+        return x -= y;
+    }
+
+    ModInt operator*(const ModInt &y) const {
+        ModInt x = *this;
+        return x *= y;
+    }
+
+    ModInt operator%(const ModInt &y) const {
+        ModInt x = *this;
+        return x %= y;
+    }
+
+    ModInt binpow(const ModInt &y, ll pow) const {
+        pow %= mod - 1;
+        ModInt res = 1, a = y;
+        while (pow) {
+            if (pow & 1) res *= a;
+            a *= a, pow >>= 1;
+        }
+        return res;
+    }
+
+    ModInt inv() const { return binpow(*this, mod - 2); }
+
+    ModInt &operator/=(const ModInt &y) {
+        p = (p * y.inv().p) % mod;
+        return *this;
+    }
+
+    ModInt operator/(const ModInt &y) const {
+        ModInt x = *this;
+        return x /= y;
+    }
+
+    friend istream &operator>>(istream &is, ModInt &a) {
+        int v;
+        is >> v;
+        a = ModInt(v);
+        return is;
+    }
+
+    friend ostream &operator<<(ostream &os, const ModInt &a) { return os << a.p; }
+
+    ModInt &operator++() {
+        p = (p + 1) % mod;
+        return *this;
+    }
+
+    ModInt &operator--() {
+        p = (p - 1 + mod) % mod;
+        return *this;
+    }
+
+    bool operator==(const ModInt &y) const { return p == *y; }
+
+    bool operator!=(const ModInt &y) const { return p != *y; }
+
+    const ll &operator*() const { return p; }
+
+    ll &operator*() { return p; }
+
+};
+
+using Mint = ModInt<>;
+#define vmint vector<Mint>
+#define vvmint vector<vector<Mint>>
+typedef tree<ll, null_type, less_equal<>, rb_tree_tag, tree_order_statistics_node_update> oSet;
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#pragma GCC target("popcnt")
+#define vct vector
+
+int cntLeq(vll &v, ll x) { return std::upper_bound(v.begin(), v.end(), x) - v.begin(); }
+int cntLess(vll &v, ll x) { return std::lower_bound(v.begin(), v.end(), x) - v.begin(); }
+int cntGreater(vll &v, ll x) { return v.end() - std::upper_bound(v.begin(), v.end(), x); }
+int cntGeq(vll &v, ll x) { return v.end() - std::lower_bound(v.begin(), v.end(), x); }
+
+#define int long long int
+//////////////////////////////////////////////////////////////////////////
+
+
+
+signed main() {
+    IO;
+}
