6dff1a4e25e3a1e5a1b525d3346de01192ee6282
HUECTRUM <plyusnovdmitrii@gmail.com>
2025-04-27 04:55:25 +0200

    linsieve

--- math/sieve/mult_function.cpp
+++ math/sieve/mult_function.cpp
@@ -1,41 +1,5 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 #include <bits/stdc++.h>
-
 using namespace std;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-
-/* TYPES  */
-#define ll long long
-
-#define vll vector<long long>
-
-
-typedef tree<ll, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> oSet;
-#define IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#pragma GCC target("popcnt")
-
-
-template<typename T>inline void chmax(T &a,T b){a=max(a,b);}
-template<typename T>inline void chmin(T &a,T b){a=min(a,b);}
-
-void vectorCoordinateCompression(vll &v) {
-    std::sort(v.begin(), v.end());
-    v.erase(std::unique(v.begin(), v.end()), v.end());
-}
-
-int getVectorCompressed(ll val, vll &v) { return lower_bound(v.begin(), v.end(), val) - v.begin(); }
-
-ll ndivto(ll n, ll k) { return n / k; }
-ll ndivfrom(ll n, ll k) { return ndivto(n, k + 1) + 1; }
-
-ll ndivfromCeil(ll n, ll k) { return n / k + (n % k ? 1 : 0); }
-ll ndivtoCeil(ll n, ll k) { return ndivfromCeil(n, k - 1) - 1; }
-
-
-//////////////////////////////////////////////////////////////////////////
-#define int long long int
 
 
 const int MAXN = 1e6 + 10;
@@ -82,18 +46,5 @@ void sieve(int n = MAXN) {
                 lp[i * primes[j]] = primes[j];
             }
         }
-    }
-}
-
-
-signed main() {
-    IO;
-
-    sieve();
-
-    int t; cin >> t;
-    while (t--) {
-        int n; cin >> n;
-        cout << func[n] - n << endl;
     }
 }
---
+++ math/sieve/verification.md
@@ -0,0 +1,6 @@
+Linear sieve and values of multiplicative functions
+
+## Sum of Totient Function
+Link: [Problem](https://judge.yosupo.jp/problem/sum_of_totient_function)
+
+Code: [boop](https://judge.yosupo.jp/submission/281863)
