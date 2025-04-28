547d4289f3136ecb4b44b936605eff7c2cdee5ac
HUECTRUM <plyusnovdmitrii@gmail.com>
2025-04-28 22:21:18 +0200

    li ciao

--- data_structures/li_chao/verification.cpp
+++ data_structures/li_chao/verification.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long int
+
+
+
 struct lchUpdate { int v, ver; pair<int, int> val; };
 template<int INF = 5000000000000000000> struct LiChaoMax {
     int n;
@@ -91,3 +95,101 @@ template<int INF = 5000000000000000000> struct LiChaoMin {
         }
     }
 };
+
+const int INF = 5e18;
+
+struct addLine {int l, r; pair<int, int> line; };
+struct DynConn {
+    int q;
+    vector<vector<pair<int, int>>> stree;
+    vector<int> queryFilter, answers;
+    int lastVer = 1;
+    LiChaoMax<> lch;
+
+    DynConn(LiChaoMax<> &liChaoMax): lch(liChaoMax) {}
+
+    void add(int l, int r, pair<int, int> val, int v, int tl, int tr) {
+        if (l > r) return;
+        if (l == tl && r == tr) return void(stree[v].push_back(val));
+
+        int tm = (tl + tr) >> 1;
+        add(l, min(r, tm), val, v << 1, tl, tm);
+        add(max(l, tm + 1), r, val, v << 1 | 1, tm + 1, tr);
+    }
+
+    void add(int l, int r, pair<int, int> val) { add(l, r, val, 1, 0, q - 1); }
+
+    void dfs(int v, int tl, int tr) {
+        int currVer = lastVer; ++lastVer;
+        for (auto ln: stree[v]) lch.insert(ln, currVer);
+
+        if (tl == tr) {
+            if (queryFilter[tl] != INF) answers[tl] = lch.queryUncompr(queryFilter[tl]);
+        }
+        else {
+            int tm = (tl + tr) >> 1;
+            dfs(v << 1, tl, tm); dfs(v << 1 | 1, tm + 1, tr);
+        }
+
+        lch.rollbackTo(currVer);
+    }
+
+    void dfs() { dfs(1, 0, q - 1); }
+
+    void init(int queries, vector<addLine> &allLines, vector<int> &quFilter) {
+        q = queries, queryFilter = quFilter;
+        stree = vector<vector<pair<int, int>>>(4 * q); answers = vector<int>(q);
+        for (auto &[lineL, lineR, line]: allLines) add(lineL, lineR, line);
+    }
+
+    void run() { dfs(); }
+};
+
+void vectorCoordinateCompression(vector<int> &v) {
+    std::sort(v.begin(), v.end());
+    v.erase(std::unique(v.begin(), v.end()), v.end());
+}
+
+signed main() {
+    vector<int> allCoords;
+
+    int q; cin >> q;
+
+    vector<addLine> lines;
+    map<int, pair<int, int>> startTimes;
+    vector<int> queryFilter(q, INF);
+
+    for (int i = 0; i < q; ++i) {
+        int type, a, b; cin >> type;
+        if (type == 1) {
+            cin >> a >> b;
+            startTimes[i] = {a, b};
+        }
+        else if (type == 2) {
+            cin >> a, --a;
+            pair<int, int> line = startTimes[a]; startTimes.erase(a);
+            lines.push_back({a, i, line});
+        }
+        else {
+            cin >> a, allCoords.push_back(a);
+            queryFilter[i] = a;
+        }
+    }
+    for (auto [st, ln]: startTimes) lines.push_back({st, q - 1, ln});
+
+    allCoords.push_back(-2e9), allCoords.push_back(2e9);
+    vectorCoordinateCompression(allCoords);
+
+
+    LiChaoMax lch(allCoords);
+    DynConn dc(lch);
+    dc.init(q, lines, queryFilter);
+    dc.run();
+
+    vector<int> ans = dc.answers;
+    for (int i = 0; i < q; ++i) if (queryFilter[i] != INF) {
+            int curAns = ans[i];
+            if (curAns == -INF) cout << "EMPTY SET\n";
+            else cout << curAns << "\n";
+        }
+}
---
+++ data_structures/li_chao/verification.md
@@ -0,0 +1,14 @@
+## Monster Game II
+Link: [Problem](https://cses.fi/problemset/task/2085/)
+
+Code: [boop](https://cses.fi/paste/4646f328caf8bd62c272de/)
+
+## Line Add Get Min
+Link: [Problem](https://judge.yosupo.jp/problem/line_add_get_min)
+
+Code: [boop](https://judge.yosupo.jp/submission/282051)
+
+## Lena and Queries
+Link: [Problem](https://codeforces.com/contest/678/problem/F)
+
+Code: [boop](https://codeforces.com/contest/678/submission/317673256)
