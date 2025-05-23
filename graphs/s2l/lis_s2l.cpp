5ab7e2213f2a2e5a72ef2e37651448753701ae21
HUECTRUM <plyusnovdmitrii@gmail.com>
2025-05-23 04:00:41 +0200

    s2l

---
+++ graphs/s2l/lis_s2l.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T> inline void chmax(T &a, T b) { a = max(a, b); }
+template<typename T> inline void chmin(T &a, T b) { a = min(a, b); }
+
+template<typename R, typename D> struct s2lContainer {
+    virtual void addVertex(int v, D x) = 0;
+    virtual void merge(int ch, s2lContainer &other, int v, D x) = 0;
+    virtual R getResult(int v) = 0;
+};
+
+const int INF = 1e9;
+struct LISContainer : s2lContainer<int, int> {
+    vector<int> inc, dec;
+    int best = 0;
+
+    void addVertex(int v, int x) {
+        auto it = lower_bound(inc.begin(), inc.end(), x);
+        if (it == inc.end()) inc.push_back(x);
+        else *it = x;
+
+        it = lower_bound(dec.begin(), dec.end(), x, greater<>());
+        if (it == dec.end()) dec.push_back(x);
+        else *it = x;
+
+        chmax(best, (int)max(inc.size(), dec.size()));
+    }
+
+    void merge(int ch, s2lContainer<int,int> &otherBase, int v, int x) {
+        auto& other = static_cast<LISContainer&>(otherBase);
+
+        for (int i = 0; i < other.dec.size(); ++i) {
+            int pos = lower_bound(inc.begin(), inc.end(), other.dec[i]) - inc.begin();
+            if (pos) chmax(best, i + 1 + pos);
+        }
+        for (int i = 0; i < other.inc.size(); ++i) {
+            int pos = lower_bound(dec.begin(), dec.end(), other.inc[i], greater<>()) - dec.begin();
+            if (pos) chmax(best, i + 1 + pos);
+        }
+
+        if (inc.size() < other.inc.size()) inc.resize(other.inc.size(), INF);
+        for (int i = 0; i < other.inc.size(); ++i) chmin(inc[i], other.inc[i]);
+
+        if (dec.size() < other.dec.size()) dec.resize(other.dec.size(), -INF);
+        for (int i = 0; i < other.dec.size(); ++i) dec[i] = max(dec[i], other.dec[i]);
+
+        chmax(best, (int)max(inc.size(), dec.size()));
+        addVertex(v, x);
+    }
+
+    int getResult(int v) { return best; }
+};
+
+
+template<typename R, typename D, class Bucket> struct S2L {
+    int n;
+    const vector<vector<int>>& g;
+    const vector<D>& val;
+    vector<int> sz;
+    vector<R> ans;
+    vector<Bucket*> rep;
+
+    S2L(const vector<vector<int>>& g, const vector<D>& val): n(g.size()), g(g), val(val), sz(n, 1), rep(n), ans(n) { }
+
+    void dfsSz(int v, int p = -1) {
+        for (int u: g[v]) if (u != p) dfsSz(u, v), sz[v] += sz[u];
+    }
+
+
+    void dfs(int v, int p) {
+        int big = -1, mx = -1;
+        for (int u : g[v]) if (u != p && sz[u] > mx) mx = sz[u], big = u;
+
+        for (int u : g[v]) if (u != p && u != big) dfs(u, v);
+
+        if (big != -1) dfs(big, v), rep[v] = rep[big];
+        else rep[v] = new Bucket();
+
+        rep[v]->addVertex(v, val[v]);
+
+        for (int u : g[v]) if (u != p && u != big) rep[v]->merge(u, *rep[u], v, val[v]);
+
+        ans[v] = rep[v]->getResult(v);
+    }
+
+    vector<R> run(int root = 0) {
+        dfsSz(root);
+        dfs(root, -1);
+        return ans;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<int> a(n); for (int i = 0; i < n; ++i) cin >> a[i];
+    vector<vector<int>> g(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v; cin >> u >> v; --u; --v;
+        g[u].push_back(v); g[v].push_back(u);
+    }
+
+    S2L<int, int, LISContainer> runner(g, a);
+    vector<int> ans = runner.run();
+
+    int fin = 0;
+    for (auto &x: ans) chmax(fin, x);
+    cout << fin;
+}
---
+++ graphs/s2l/sack_timer.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+void vectorCoordinateCompression(vector<int> &v) {
+    std::sort(v.begin(), v.end());
+    v.erase(std::unique(v.begin(), v.end()), v.end());
+}
+
+int getVectorCompressed(int val, vector<int> &v) { return lower_bound(v.begin(), v.end(), val) - v.begin(); }
+
+template<typename R> struct s2lContainer {
+    virtual void add(int v) = 0;
+    virtual void remove(int v) = 0;
+    virtual R getResult(int v) = 0;
+};
+
+struct UniqueOps: s2lContainer<int> {
+    vector<int> cnt, col;
+    int uCnt = 0;
+
+    UniqueOps(vector<int> const &col) : cnt(col.size()), col(col) { }
+
+    void add(int v) {
+        if (!cnt[col[v]]) ++uCnt;
+        cnt[col[v]]++;
+    }
+
+    void remove(int v) {
+        if (cnt[col[v]] == 1) --uCnt;
+        cnt[col[v]]--;
+    }
+
+    int getResult(int v) { return uCnt; }
+};
+
+template<typename R, int N = 200000> struct Sack {
+    s2lContainer<R> &ds;
+    int n;
+    const vector<vector<int>> &g;
+    vector<int> sz, st, ft, ver, ans;
+    int timer = 0;
+
+    Sack(const vector<vector<int>> &g, s2lContainer<R> &ds): ds(ds), n(g.size()), g(g), sz(n), ans(n), st(n), ft(n), ver(n) {}
+
+    void dfsSz(int v, int p = -1) {
+        st[v] = timer, sz[v] = 1;
+        ver[timer++] = v;
+        for (auto &to: g[v]) if (to != p) dfsSz(to, v), sz[v] += sz[to];
+        ft[v] = timer;
+    }
+
+    void dfs(int v, int p, bool keep) {
+        int mx = -1, big = -1;
+        for (auto &u: g[v]) if (u != p && sz[u] > mx) mx = sz[u], big = u;
+
+        for (auto &u: g[v]) if (u != p && u != big) dfs(u, v, 0);
+        if (big != -1) dfs(big, v, 1);
+
+        for (auto &u: g[v]) if (u != p && u != big) {
+                for (int tm = st[u]; tm < ft[u]; ++tm) ds.add(ver[tm]);
+            }
+        ds.add(v);
+        ans[v] = ds.getResult(v);
+
+        if (!keep) for (int tm = st[v]; tm < ft[v]; ++tm) ds.remove(ver[tm]);
+    }
+
+    vector<R> run(int root = 0) {
+        dfsSz(root);
+        dfs(root, -1, 1);
+        return ans;
+    }
+};
+
+
+signed main() {
+    int n; cin >> n;
+    vector<int> col(n);
+    vector<int> allCoords;
+    for (int i = 0; i < n; ++i) cin >> col[i], allCoords.push_back(col[i]);
+
+    vectorCoordinateCompression(allCoords);
+    for (int i = 0; i < n; ++i) col[i] = getVectorCompressed(col[i], allCoords);
+
+    vector<vector<int>> g(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b; cin >> a >> b; --a, --b;
+        g[a].push_back(b); g[b].push_back(a);
+    }
+    UniqueOps op(col);
+    Sack s(g, op);
+    vector<int> ans = s.run();
+
+    for (int i = 0; i < n; ++i) cout << ans[i] << " ";
+}
