77bca7789962a5a241abc9151734783ad7084ff0
HUECTRUM <plyusnovdmitrii@gmail.com>
2025-05-25 02:05:58 +0200

    eul_path

--- graphs/eul_path/directional.cpp
+++ graphs/eul_path/directional.cpp
@@ -9,31 +9,33 @@ struct EulRes {
     vector<int> edges;
 };
 
-struct EulPath {
+struct DirEulPath {
     int n, m;
     vector<vector<pair<int, int>>> g;
-    vector<int> deg, used;
+    vector<int> inDeg, outDeg, used;
     int st = -1, en = -1;
     bool cycExists = true, pathExists = true;
     EulRes res = {true, true, {}, {}};
 
-    EulPath(vector<vector<pair<int, int>>> g) : n(g.size()), g(g), deg(n), m(0) { }
+    DirEulPath(vector<vector<pair<int, int>>> g) : n(g.size()), g(g), inDeg(n), outDeg(n), m(0) { }
 
     void runChecks() {
-        for (int i = 0; i < n; ++i) for (auto &[to, _]: g[i]) deg[to]++;
+        for (int i = 0; i < n; ++i) for (auto &[to, _]: g[i]) inDeg[to]++, outDeg[i]++, m++;
 
-        int cnt = 0;
+        int stCnt = 0, enCnt = 0;
         for (int i = 0; i < n; ++i) {
-            m += deg[i];
-
-            if (deg[i] % 2 == 0) continue;
-
-            ++cnt;
-            if (st == -1) st = i;
-            else if (en == -1) en = i;
+            if (inDeg[i] == outDeg[i]) continue;
+            if (inDeg[i] - outDeg[i] == 1) ++enCnt, en = i;
+            else if (outDeg[i] - inDeg[i] == 1) ++stCnt, st = i;
+            else cycExists = false, pathExists = false;
         }
-        cycExists = (cnt == 0), pathExists = (cnt == 0 || cnt == 2);
-        m /= 2; used = vector<int>(m);
+        if (!stCnt && !enCnt) cycExists = true;
+        else {
+            cycExists = false;
+            if (stCnt == 1 && enCnt == 1) pathExists = true;
+            else pathExists = false;
+        }
+        used = vector<int>(m);
     }
 
     void dfs(int v) {
@@ -49,8 +51,8 @@ struct EulPath {
 
     int findStart() {
         if (st != -1) return st;
-        auto it = std::find_if(deg.begin(), deg.end(), [](int d){ return d > 0; });
-        return it == deg.end() ? 0 : it - deg.begin();
+        auto it = std::find_if(outDeg.begin(), outDeg.end(), [](int d){ return d > 0; });
+        return it == outDeg.end() ? 0 : it - outDeg.begin();
     }
 
     void getCycle(int v0 = 0) { runEul(v0, true); }
@@ -60,9 +62,7 @@ struct EulPath {
         if (!cycExists && !pathExists) return void(res = {false});
         if (cycExists) st = v0;
         else {
-            if (forceCyc) return void(res = {false});
-            if (v0 != st && v0 != en) return void(res = {false});
-            if (v0 == en) swap(st, en);
+            if (forceCyc || v0 != st) return void(res = {false});
         }
 
         if (!m) {
@@ -85,11 +85,17 @@ signed main() {
     vector<vector<pair<int, int>>> g(n);
     for (int i = 0; i < m; ++i) {
         int a, b; cin >> a >> b; --a, --b;
-        g[a].push_back({b, i}); g[b].push_back({a, i});
+        g[a].push_back({b, i});
     }
-    EulPath ep(g);
+    DirEulPath ep(g);
     ep.runChecks();
-    ep.getCycle(0);
+
+    if (ep.cycExists || !ep.pathExists || ep.st != 0 || ep.en != n - 1) {
+        cout << "IMPOSSIBLE";
+        return 0;
+    }
+
+    ep.getPath(0);
     vector<int> ans = ep.res.nodes;
     if (!ep.fullExists()) cout << "IMPOSSIBLE";
     else for (auto &t: ans) cout << t + 1 << " ";
