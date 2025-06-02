a03379c7d4f9076b2bf08871adf9f31c818cd4bd
HUECTRUM <plyusnovdmitrii@gmail.com>
2025-06-02 02:38:00 +0200

    linsieve

--- math/sieve/linsieve.cpp
+++ math/sieve/linsieve.cpp
@@ -6,6 +6,7 @@ const int MAXN = 1e7 + 10;
 vector<int> lp(MAXN + 1, 0), pr;
 
 void linsieve() {
+    lp[1] = 1;
     for (int i = 2; i <= MAXN; ++i) {
         if (!lp[i]) lp[i] = i, pr.push_back(i);
         for (int j = 0; i * pr[j] <= MAXN; ++j) {
@@ -13,4 +14,15 @@ void linsieve() {
             if (pr[j] == lp[i]) break;
         }
     }
+}
+
+vector<pair<int, int>> factorize(int n) {
+    vector<pair<int, int>> ans;
+    while (n != lp[n]) {
+        int curDiv = lp[n], cnt = 0;
+        while (lp[n] == curDiv) n /= curDiv, cnt++;
+        ans.emplace_back(curDiv, cnt);
+    }
+    if (n > 1) ans.emplace_back(n, 1);
+    return ans;
 }
